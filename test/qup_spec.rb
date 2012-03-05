34ebdb8f1b4b2a54c3a65d79b9fdd39a790a86e9
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-03-04 18:34:52 -0700

    fix version test

--- test/qup_spec.rb
+++ test/qup_spec.rb
@@ -3,10 +3,6 @@ require "qup"
 
 describe Qup do
   it "should have a version" do
-    qup::VERSION.must_match( %r[\A\d+\.\d+\.\d+\Z] )
-  end
-
-  it "should have some tests or I will kneecap you" do
-    flunk
+    Qup::VERSION.must_match( %r[\A\d+\.\d+\.\d+\Z] )
   end
 end
