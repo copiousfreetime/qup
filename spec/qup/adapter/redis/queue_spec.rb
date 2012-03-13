9ba5e5787b37b24741eb213c1b4bad305616c4c8
Nathan Witmer <nwitmer@gmail.com>
2012-03-12 19:35:19 -0600

    Redis Queue can remove itself from a parent topic's subscriber set
    If initialized with a parent topic's name, a Redis Queue will remove itself from
    the topic's subscriber set.

--- spec/qup/adapter/redis/queue_spec.rb
+++ spec/qup/adapter/redis/queue_spec.rb
@@ -6,4 +6,28 @@ describe 'Qup::Adapter::Redis::Queue', :redis => true do
   include_context "Qup::Adapter::Redis"
   include_context "Qup::Queue"
   it_behaves_like Qup::QueueAPI
+
+  context "when initialized with a parent topic's name" do
+    let(:redis) { Redis.new :host => uri.host, :port => uri.port }
+    let(:queue) { Qup::Adapter::Redis::Queue.new(uri, "test", "parent") }
+
+    before do
+      redis.del "parent"
+      redis.sadd "parent", "test"
+    end
+
+    after do
+      redis.del "parent"
+      redis.client.disconnect
+    end
+
+    describe "#destroy" do
+      it "removes its name from the parent topic's subscriber set" do
+        redis.smembers("parent").should == ["test"]
+        queue.destroy
+        redis.smembers("parent").should == []
+      end
+    end
+
+  end
 end
