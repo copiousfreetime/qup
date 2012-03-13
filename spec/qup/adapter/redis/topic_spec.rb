6c7c1c90987ce1be5e6077709511500135507367
Nathan Witmer <nwitmer@gmail.com>
2012-03-12 19:38:16 -0600

    Use a Redis set for tracking Topic subscriptions
    Rather than using an in-memory hash, which limits subscription publishing to a
    single process. Using a set to track all subscribers means multiple processes
    can subscribe to the same topic.

--- spec/qup/adapter/redis/topic_spec.rb
+++ spec/qup/adapter/redis/topic_spec.rb
@@ -6,4 +6,24 @@ describe 'Qup::Adapter::Redis::Topic', :redis => true  do
   include_context "Qup::Adapter::Redis"
   include_context "Qup::Topic"
   it_behaves_like Qup::TopicAPI
+
+  describe "#subscriber" do
+
+    let(:topic)      { adapter.topic "test" }
+    let(:subscriber) { topic.subscriber "testing" }
+
+    before do
+      topic && subscriber # create and register the subscriber
+    end
+
+    after do
+      topic.destroy
+    end
+
+    it "unregisters itself from the Topic when unsubscribed" do
+      lambda do
+        subscriber.unsubscribe
+      end.should change(topic, :subscriber_count).by(-1)
+    end
+  end
 end
