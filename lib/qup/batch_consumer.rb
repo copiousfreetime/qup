b15ec2cd429f32ddbabef6fe1e6a86c1c767bfa4
Rafer Hazen <rafer@ralua.com>
2012-08-29 11:54:44 -0600

    Add BatchConsumer documentation.

--- lib/qup/batch_consumer.rb
+++ lib/qup/batch_consumer.rb
@@ -1,3 +1,49 @@
+# BatchConsumer makes it easy to implement a batch-oriented consumer pattern
+# by calling the appropriate hooks on the provided "client" class. Clients
+# have the following callbacks:
+#
+# setup - Optional. Called before the client begins receiving messages
+#   (immediately after BatchConsumer#run is called).
+# process - Required. Called with an instance of Qup::Message when a
+#   message is removed from the queue. After process finishes the message is
+#   acknowledged from the queue. process is not wrapped in any exception
+#   handling, so exceptions raised in process will propagate through the
+#   BatchConsumer and the message will not be acknowledged.
+# teardown - Optional. Called after the client has received :max_size
+#   messages OR :max_age has been exceeded.
+#
+# BatchConsumers should be only be run once. If you need to run aain, create
+# a new instance.
+#
+# See #initialize for the config options.
+#
+# Examples:
+#
+#   class FileDumper
+#     include Qup::BatchConsumerAPI
+#
+#     def setup
+#       @file = File.open("/my/file.txt")
+#     end
+#
+#     def process(message)
+#       @file.puts(message.data)
+#     end
+#
+#     def teardown
+#       @file.close
+#     end
+#   end
+#
+#   batch_consumer = Qup::BatchConsumer.new({
+#     :max_size   => 5000,
+#     :max_age    => 600,
+#     :client     => FileDumper.new,
+#     :queue_uri  => "maildir:///tmp/test-queue",
+#     :queue_name => "my-queue"
+#   })
+#
+#   batch_consumer.run # This blocks until there have been 5000 message OR 600 seconds have passed.
 module Qup
   module BatchConsumerAPI
     def setup
@@ -12,7 +58,23 @@ module Qup
   end
 
   class BatchConsumer
-
+    # options - A hash of configuration options.
+    #          :client - The object upon which the callbacks are fired. It's class
+    #                    should include Qup::BatchConsumerAPI for declarative
+    #                    documentation purposes. Technically, the only constraint
+    #                    is that this object implement #setup, #process and
+    #                    #teardown (including Qup::BatchConsumerAPI includes noop
+    #                    definitions for #setup and #teardown). Required.
+    #          :queue_uri - The Qup format queue URI. Required.
+    #          :queue_name - The name of the queue that messages will be consumed
+    #                        from. Required.
+    #          :max_size - The maximum number of messages to process. Optional.
+    #          :max_age - The maximum number of seconds (from when #run is called)
+    #                     before finishing. Note that :max_size and :max_age are
+    #                     ORed, meaning that the BatchConsumer will finish when
+    #                     the first constraint is met. If neither constraint is
+    #                     provided, the BatchConsumer will never finish (i.e. #run
+    #                     will block indefinitely). Optional.
     def initialize(config = {})
       @message_count = 0
       @config        = config
