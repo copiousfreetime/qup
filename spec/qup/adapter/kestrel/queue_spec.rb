2fe45564a2061f4394853df13e438d3ccda1ab2c
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-10-31 11:22:17 -0600

    remove unneeded tests now that we use kjess

--- spec/qup/adapter/kestrel/queue_spec.rb
+++ spec/qup/adapter/kestrel/queue_spec.rb
@@ -6,14 +6,4 @@ describe 'Qup::Adapter::Kestrel::Queue', :kestrel => true do
   include_context "Qup::Adapter::Kestrel"
   include_context "Qup::Queue"
   it_behaves_like Qup::QueueAPI
-
-  it "sets client options in the queue" do
-    pending "Replacement of kestrel client"
-    Qup::Session.new(uri.to_s, :client => {:the => "OPTION"}).queue("test-queue").instance_variable_get("@client").instance_variable_get("@options")[:the].should == "OPTION"
-  end
-
-  it "sets client options in the topic" do
-    pending "Replacement of kestrel client"
-    Qup::Session.new(uri.to_s, :client => {:the => "OPTION"}).topic("test-queue").instance_variable_get("@client").instance_variable_get("@options")[:the].should == "OPTION"
-  end
 end
